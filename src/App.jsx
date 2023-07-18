d644fdd61c368eca8ecb9a63eabd3b71cd6f35cb
trineesha thathsarani <trineeshasanduni125@gmail.com>
2023-07-18 16:49:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import '@fontsource/poppins';
 import NavBar from './components/navbar';
 import Landing from './components/landingPage';
 import CardList from './components/cards';
+import SideBar from './components/sidebar';
 
 export default function App() {
   return (
     <>
+    {/* <NavBar/>
     <Landing/>
+    <CardList/> */}
+    <SideBar/>
+    {/* <div className="bg-indigo-500 w-full h-screen"></div> */}
     </>
   )
 }
