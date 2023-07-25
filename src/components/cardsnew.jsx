a96d1c29c711cb72fabceed6d02f2b11cf7e5f85
ravefx <ravindhuprabasha@gmail.com>
2023-07-25 22:42:06 +0530

    update to landing

--- src/components/cardsnew.jsx
+++ src/components/cardsnew.jsx
@@ -2,48 +2,55 @@ import React from "react";
 
 const cards = [
   {
-    name: "Wooden House, Florida",
+    name: "Sigiriya",
     description:
       "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka.",
     rating: 5,
-    features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
+    hashtag1: "heritage",
+    hashtag2: "history",
     image: "./cardimage.jpg",
   },
 
   {
-    name: "Wooden House, Florida",
+    name: "Ella",
     description:
-      "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka.",
+      "Small town in the Badulla District of Uva Province, Sri Lanka. The area has a rich bio-diversity, dense with numerous varieties of flora and fauna",
     rating: 5,
     features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
-    image: "A:UCSC/3RDYEAR/travo-tms/travo-tms/public/cardimage.jpg",
+    image: "/ella.jpg",
+    hashtag1: "upcountry",
+    hashtag2: "mountains",
   },
 
   {
-    name: "Wooden House, Florida",
+    name: "Dunhinda Falls",
     description:
-      "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka.",
+      "Dunhinda Falls is a waterfall located about 5 kilometres from Badulla in the lower central hills of Sri Lanka. The waterfall gets its name from the smoky dew drops.",
     rating: 5,
-    features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
-    image: "A:UCSC/3RDYEAR/travo-tms/travo-tms/public/cardimage.jpg",
+    hashtag1: "upcountry",
+    hashtag2: "nature",
+    image: "/dunhinda.jpg",
   },
 
   {
-    name: "Wooden House, Florida",
+    name: "Hortan Plains",
     description:
-      "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka.",
+      "Located at an elevation of 2,100–2,300 m (6,900–7,500 ft) and encompasses montane grassland and cloud forest. It is rich in biodiversity and many species",
     rating: 5,
-    features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
-    image: "A:UCSC/3RDYEAR/travo-tms/travo-tms/public/cardimage.jpg",
+    hashtag1: "biodiversity",
+    hashtag2: "wildlife",
+    image: "/hortan.jpg",
   },
 
   {
-    name: "Wooden House, Florida",
+    name: "Sri Pada (Adam's Peak)",
     description:
-      "Sigiriya or Sinhagiri is an ancient rock fortress located in the northern Matale District near the town of Dambulla in the Central Province, Sri Lanka.",
+      "Adam's Peak is a 2,243 m (7,359 ft) tall conical sacred mountain located in central Sri Lanka.It is well known for the Sri Pada (Śrī Pāda; Sinhala: ශ්‍රී පාද, sacred footprint)",
     rating: 5,
-    features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
-    image: "A:UCSC/3RDYEAR/travo-tms/travo-tms/public/cardimage.jpg",
+    // features: ["Free wifi", "2 bedrooms", "65 HDTV", "Fire alert"],
+    hashtag1: "buddhism",
+    hashtag2: "mountains",
+    image: "/adams.jpg",
   },
 ];
 
@@ -54,24 +61,27 @@ export default function Newcards() {
         <a href="#" class="block w-full h-full">
           <img
             alt="blog photo"
-            src="/cardimage.jpg"
+            src={`${card.image}`}
             class="object-cover w-full max-h-40"
             className="rounded-t-lg"
           />
           <div class="w-full rounded-b-lg p-4 bg-white dark:bg-gray-800">
-            <p class="font-medium text-indigo-500 text-md">Article</p>
+            <button className=" bg-rose-700 p-1 rounded-md font-poppins font-sm text-sm text-white">
+              Trending
+            </button>
+            {/* <p class="font-medium text-indigo-500 text-md">Article</p> */}
             <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
-              Sigiriya
+              {card.name}
             </p>
             <p class="font-light text-gray-400 dark:text-gray-300 text-md">
               {card.description}
             </p>
             <div class="flex flex-wrap items-center mt-4 justify-starts">
               <div class="text-xs mr-2 py-1.5 px-4 text-gray-600 bg-blue-100 rounded-2xl">
-                #Car
+                #{card.hashtag1}
               </div>
               <div class="text-xs mr-2 py-1.5 px-4 text-gray-600 bg-blue-100 rounded-2xl">
-                #Money
+                #{card.hashtag2}
               </div>
             </div>
           </div>
--- src/components/newnav.jsx
+++ src/components/newnav.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Newnav() {
   return (
-    <div class="font-poppins">
-      <nav class="  bg-opacity-0 fixed z-20 top-0 w-screen h-15 left-0 ">
+    <div class="font-poppins ">
+      <nav class="  bg-transparent fixed z-20 top-0 w-screen h-15 left-0 ">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="https://flowbite.com/" class="flex items-center">
+          <a href="https://ravindurave.me/" class="flex items-center">
             <img
               src="/travo-black.png"
               class="h-8 mr-3"
@@ -18,7 +18,7 @@ function Newnav() {
           <div class="flex md:order-2">
             <button
               type="button"
-              class="text-white  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-white  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-indigo-800 dark:hover:bg-indigo-950 dark:focus:ring-blue-800"
             >
               Get started
             </button>
@@ -51,11 +51,11 @@ function Newnav() {
             class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:border-gray-700">
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  class="block py-2 pl-3 pr-4 text-white  rounded md:bg-transparent md:text-gray-700 md:p-0 md:dark:text-gray-950"
                   aria-current="page"
                 >
                   Home
@@ -64,7 +64,7 @@ function Newnav() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-black-700 md:p-0 md:dark:hover:text-blue-950 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   About
                 </a>
@@ -72,7 +72,7 @@ function Newnav() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-950 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Services
                 </a>
@@ -80,7 +80,7 @@ function Newnav() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-950 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
                 </a>
