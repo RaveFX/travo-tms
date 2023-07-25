9e066ea316341d7df4841b02507049ca9b6afeb0
Ravindu Viranga <ravinduviranga20@gmail.com>
2023-07-25 15:23:04 +0530

    table create

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import '@fontsource/poppins';
 import NavBar from './components/navbar';
 import Landing from './components/landingPage';
 import CardList from './components/cards';
+import SideBar from './components/sidebar';
+import Tables from './components/table';
 //import SideBar from './components/sidebar';
 
 export default function App() {
   return (
     <>
     <NavBar/>
-    <Landing/>
-    <CardList/>
+    
+    <Tables/>
     {/* <SideBar/> */}
     <div className="bg-indigo-500 w-full h-screen"></div>
     </>
