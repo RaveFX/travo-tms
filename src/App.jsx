9ddecef3fad0cae477a80d149679fb55e20b24e7
Ravindu Viranga <ravinduviranga20@gmail.com>
2023-07-27 00:34:12 +0530

    table

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import Landing from './components/landingPage';
 import CardList from './components/cards';
 import SideBar from './components/sidebar';
 import Tables from './components/table';
+<<<<<<< Updated upstream
 import Schedule from './components/schedule';
+=======
+import TopNavbar from './components/topNavbar';
+>>>>>>> Stashed changes
 //import SideBar from './components/sidebar';
 
 export default function App() {
@@ -13,7 +17,11 @@ export default function App() {
     <NavBar/>
     
     <Tables/>
+<<<<<<< Updated upstream
     <Schedule/>
+=======
+    <TopNavbar/>
+>>>>>>> Stashed changes
     {/* <SideBar/> */}
     <div className="bg-indigo-500 w-full h-screen"></div>
     </>
