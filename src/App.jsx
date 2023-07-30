cb6eb0a788ccf1787e718351312dceb960b52599
Tharindi Senadeera <tia.tharindi@gmail.com>
2023-07-30 11:15:08 +0530

    signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import '@fontsource/poppins';
-import NavBar from './components/navbar';
-import Landing from './components/landingPage';
-import CardList from './components/cards';
-//import SideBar from './components/sidebar';
+import "@fontsource/poppins";
+// import NavBar from "./components/navbar";
+import Newnav from "./components/newnav";
+//import Landing from "./components/landingPage";
+import Signin from "./components/signin";
+//import signup from "./components/signup";
+//import SideBar fsdfsrom './components/sidebar';
 
 export default function App() {
   return (
     <>
-    <NavBar/>
-    <Landing/>
-    <CardList/>
-    {/* <SideBar/> */}
-    <div className="bg-indigo-500 w-full h-screen"></div>
+      <Newnav />
+      <Signin />
+
+      <div className="bg-[#F6F8FA] w-full h-screen"></div>
     </>
-  )
+  );
 }
---
+++ src/components/signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Signin () {
+  return (
+    <div className="font-poppins relative overflow bg-cover bg-no-repeat w-screen h-screen text-white"
+    style={{
+      backgroundImage: "url('/hero.png')",
+      //  background: "linear-gradient(90deg, rgba(131, 126, 226, 1) 24%, rgba(114, 114, 226, 1) 58%, rgba(0, 212, 255, 1) 100%)"
+    }}>
+    <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Signin
