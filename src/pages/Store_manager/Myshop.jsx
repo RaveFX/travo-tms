8c9f1490fdca0af1dcfb0439df6665c9b61159a5
R.A.T.U.Perera <92754215+thirani99@users.noreply.github.com>
2023-08-12 01:08:49 +0530

    Store updated

---
+++ src/pages/Store_manager/Myshop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Shop from '../../components/Productcards';
+import Product_card from '../../components/product_card';
+
+export default function Myshop() {
+    return (
+        <div>
+            {/* <Shop /> */}
+            <Product_card />
+        </div>
+    )
+}
