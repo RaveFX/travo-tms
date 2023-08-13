f024d000297879a1f91f7b33885ddf9933508c99
ravefx <ravindhuprabasha@gmail.com>
2023-08-13 08:50:38 +0530

    up

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -8,9 +8,13 @@ function Store() {
     <>
       <div className="font-poppins w-full bg-[#F6F8FA] flex overflow-hidden ">
         <div class="flex">
-          <aside class="h-screen sticky top-0">// Fixed Sidebar</aside>
+          <aside class="h-screen sticky top-0">
+            <Sidebar />
+          </aside>
 
-          <main>// Content</main>
+          <main>
+            <TopNavbar />
+          </main>
         </div>
       </div>
     </>
