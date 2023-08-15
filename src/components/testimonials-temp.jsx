09a5eb0b840ad8e9c8a0a6a12b550dbafa5f96e8
Tharindi Senadeera <tia.tharindi@gmail.com>
2023-08-15 12:41:04 +0530

    updates

--- src/components/testimonials-temp.jsx
+++ src/components/testimonials-temp.jsx
@@ -22,37 +22,28 @@ const testo = [
       "Using this travel app has been a game-changer! Seamless bookings, breathtaking destinations, and excellent support.",
     image: "/hichcha.jpg",
   },
-
-  {
-    name: "Anushka Rajapaksha",
-    position: "Pahasara UCSC",
-    description:
-      "Using this travel app has been a game-changer! Seamless bookings, breathtaking destinations, and excellent support.",
-    image: "/hichcha.jpg",
-  },
 ];
 
 function Testimonials() {
   return (
-    <div className="flex flex-row gap-5 mx-10">
+    <div>
+      <div className></div>
       {testo.map((card) => (
-        <div class="  w-[25%] max-h-50 mt-10  max-w-lg p-7 text-left bg-white shadow-xl rounded-xl">
+        <div class="inline-flex  max-h-50  ml-10 mt-10  max-w-lg p-8 text-left bg-white shadow-xl rounded-xl">
           <p class="mx-auto text-base leading-relaxed text-gray-500">
             {card.description}
           </p>
           <h2 class="mt-4 text-xs font-semibold tracking-widest text-blue-500 uppercase">
             {card.name}
+            <span href="#" class="font-semibold text-gray-200 lg:mb-0">
+              {card.position}
+            </span>
           </h2>
-          <span href="#" class="font-semibold text-gray-200 lg:mb-0">
-            {card.position}
-          </span>
-          <div>
-            <img
-              alt="testimonial"
-              class="inline-block object-cover object-center w-20 h-20 mt-8 rounded-full"
-              src="/hichcha.jpg"
-            ></img>
-          </div>
+          <img
+            alt="testimonial"
+            class="inline-block object-cover object-center w-20 h-20 mt-8 rounded-full"
+            src="/hichcha.jpg"
+          ></img>
         </div>
       ))}
     </div>
