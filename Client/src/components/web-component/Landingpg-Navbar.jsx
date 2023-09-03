4cf66fae1ce1d7845e947dc259053b77feb17003
Sanduni Gunawardhana <sandugunawardhana99@gmail.com>
2023-09-03 16:37:41 +0530

    service providers

--- Client/src/components/web-component/Landingpg-Navbar.jsx
+++ Client/src/components/web-component/Landingpg-Navbar.jsx
@@ -1,18 +1,18 @@
-// src/TopNavbar.js
-import React from 'react';
-import {
-  Typography,
-} from "@material-tailwind/react";
-const TopNavbarLanding = () => {
+import React from "react";
+import { Typography } from "@material-tailwind/react";
 
-  
+const TopNavbarLanding = () => {
   return (
-    <nav className="bg-[#FFFFFF] ">
-      <div className="mx-auto w-full pr-2 pl-2 flex items-center justify-between">
-        <div className="">
-            <img src="/images/Lpg-logo.svg" alt="Logo" className="w-[112px] h-[62px]" />
+    <nav className="bg-[#FFFFFF] p-2">
+      <div className="mx-auto w-full flex items-center justify-between">
+        <div className="w-[150px] h-[50px]">
+          <img
+            src="/images/Lpg-logo.svg"
+            alt="Logo"
+            className="w-full h-full"
+          />
         </div>
-        <div className="flex space-x-[65px] font-poppins">
+        <div className="flex flex-col gap-x-10 font-poppins md:flex-row">
           <div>
             <a href="#" className="text-Black text-[#2AB57D] hover:font-bold">
               Trending
@@ -34,17 +34,18 @@ const TopNavbarLanding = () => {
             </a>
           </div>
         </div>
-        <div className='flex font-poppins'>
-          <Typography color="black" className="font-bold text-[#57CC99] flex align-center" onClick="">Become a Service Provider</Typography>
-
-        </div>
-        <div className='flex justify-end gap-2'>
-            <button className="text-Black px-4 py-2 rounded-full bg-[#2AB57D] text-[#FFFFFF]">
-             Sign in
-            </button>
-            <button className="text-Black px-4 py-2 rounded-full bg-[#2AB57D] text-[#FFFFFF]">
-             Sign up
-            </button>
+        <a href="#" className="flex font-poppins text-[#57CC99] items-center w-fit">
+          <Typography color="black" className="font-bold ">
+            Become a Service Provider
+          </Typography>
+        </a>
+        <div className="flex items-center space-x-2 md:space-x-5">
+          <button className="text-Black px-3 py-1 rounded-full bg-[#2AB57D] text-[#FFFFFF] text-s md:text-base">
+            Sign in
+          </button>
+          <button className="text-Black px-3 py-1 rounded-full bg-[#2AB57D] text-[#FFFFFF] text-s md:text-base">
+            Sign up
+          </button>
         </div>
       </div>
     </nav>
--- Client/src/pages/main/ServiceProvider.jsx
+++ Client/src/pages/main/ServiceProvider.jsx
@@ -5,85 +5,156 @@ import TopNavbarLanding from "../../components/web-component/Landingpg-Navbar";
 const BecomeServiceProvider = () => {
   return (
     <div className="bg-gray-100 h-screen ">
-      <TopNavbarLanding className="fixed"/>
-      <div className="object-contain h-[90vh] overflow-y-scroll">
+      <TopNavbarLanding className="fixed" />
+      <div
+        className="object-contain h-[90vh] overflow-y-scroll"
+        style={{ scrollbarWidth: "none" }}
+      >
         {/* <div className="flex flex-col"> */}
-          {/* <div className=" absolute top-40 left-0 right-0 w-full h-[600px] bg-black opacity-70"></div> */}
-          <div className="relative">
-            <div className="absolute top-40 left-0 right-0 text-center p-0 bg-black-100 text-white text-shadow-20 z-[1000]">
-              <h1 className="text-[100px] font-semibold mb-4">
-                Become a Service Provider
-              </h1>
-              <p className="text-white mb-8">
-                Join our platform as a service provider and offer your travel
-                planning expertise to our users.
-              </p>
-            </div>
-            
-            <video
-              className="top-1 left-0 right-0 w-full  object-cover bg-black-200"
-              autoPlay
-              loop
-              muted
-            >
-              <source src="main/video.mp4" type="video/mp4" />
-            </video>
-          </div>
 
-          <div className="flex justify-center mt-12">
-            <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-              {/* Hotel Category */}
-              <div className="p-4 bg-white rounded-lg shadow-md text-center">
-                <img
-                  src="hotel-icon.png"
-                  alt="Hotel Icon"
-                  className="mx-auto w-16 h-16 mb-2"
-                />
-                <h2 className="text-lg font-semibold mb-1">Hotel</h2>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                  Select
-                </button>
-              </div>
-              {/* Activity Agent Category */}
-              <div className="p-4 bg-white rounded-lg shadow-md text-center">
-                <img
-                  src="activity-icon.png"
-                  alt="Activity Icon"
-                  className="mx-auto w-16 h-16 mb-2"
-                />
-                <h2 className="text-lg font-semibold mb-1">Activity Agent</h2>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                  Select
-                </button>
-              </div>
-              {/* Travel Guide Category */}
-              <div className="p-4 bg-white rounded-lg shadow-md text-center">
-                <img
-                  src="guide-icon.png"
-                  alt="Guide Icon"
-                  className="mx-auto w-16 h-16 mb-2"
-                />
-                <h2 className="text-lg font-semibold mb-1">Travel Guide</h2>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                  Select
-                </button>
-              </div>
-              {/* Vehicle Renter Category */}
-              <div className="p-4 bg-white rounded-lg shadow-md text-center">
-                <img
-                  src="vehicle-icon.png"
-                  alt="Vehicle Icon"
-                  className="mx-auto w-16 h-16 mb-2"
-                />
-                <h2 className="text-lg font-semibold mb-1">Vehicle Renter</h2>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                  Select
-                </button>
-              </div>
+        <div className="relative">
+          <div className="absolute top-40 left-0 right-0 text-center p-0 bg-black-100 text-white text-shadow-20 z-[100]">
+            <h1 className="text-4xl md:text-8xl font-semibold mb-4">
+              Become a Service Provider
+            </h1>
+            <p className="text-white mb-8">
+              Join our platform as a service provider and offer your travel
+              planning expertise to our users.
+            </p>
+            <button className="bg-[#2AB57D] text-white px-4 py-2 font-semibold rounded hover:bg-[#2AB67D]">
+              Become a Service Provider
+            </button>
+          </div>
+          <div className=" absolute top-0 left-0 right-0 w-full h-[500px] z-0 object-cover bg-black opacity-70"></div>
+          <video
+            className="top-0 left-0 right-0 w-full h-[500px] z-0 object-cover bg-black-200"
+            autoPlay
+            loop
+            muted
+          >
+            <source src="main/video.mp4" type="video/mp4" />
+          </video>
+        </div>
+        <div>
+          <h4 className="flex justify-center mt-[20px] text-2xl md:text-3xl font-semibold mb-4">
+            Join our growing Service provider community
+          </h4>
+        </div>
+
+        <div className="flex justify-center mt-[20px] z-[1000]">
+          <div className="grid grid-cols-2 gap-4 m-2 md:grid-cols-4">
+            {/* Hotel Category */}
+            <div className="p-4 bg-white rounded-lg shadow-md text-center">
+              <img
+                src="main/hotelOwner.jpg"
+                alt="Hotel Icon"
+                className="mx-auto w-fit h-fit mb-2"
+              />
+              <h2 className="text-lg font-semibold mb-1">Hotel & Accomodation</h2>
+              {/* <button className="bg-[#22577A] text-white px-4 py-2 rounded hover:bg-[#22578A]">
+                Select
+              </button> */}
+            </div>
+            {/* Activity Agent Category */}
+            <div className="p-4 bg-white rounded-lg shadow-md text-center">
+              <img
+                src="main/activityAgent.jpg"
+                alt="Activity Icon"
+                className="mx-auto w-fit h-fit mb-2"
+              />
+              <h2 className="text-lg font-semibold mb-1">Activity Agent</h2>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                Select
+              </button>
+            </div>
+            {/* Travel Guide Category */}
+            <div className="p-4 bg-white rounded-lg shadow-md text-center">
+              <img
+                src="main/travelGuied.jpg"
+                alt="Guide Icon"
+                className="mx-auto w-fit h-fit mb-2"
+              />
+              <h2 className="text-lg font-semibold mb-1">Travel Guide</h2>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                Select
+              </button>
+            </div>
+            {/* Vehicle Renter Category */}
+            <div className="p-4 bg-white rounded-lg shadow-md text-center">
+              <img
+                src="main/vehicleRenter.jpg"
+                alt="Vehicle Icon"
+                className="mx-auto w-fit h-fit mb-2"
+              />
+              <h2 className="text-lg font-semibold mb-1">Vehicle Renter</h2>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                Select
+              </button>
             </div>
           </div>
         </div>
-      {/* </div> */}
+        {/* How It Works Section */}
+        <div className="bg-white mt-8 p-8 rounded shadow-md text-center">
+          <h2 className="text-3xl font-semibold mb-4">How It Works</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            {/* Step 1 */}
+            <div className="flex flex-col items-center">
+              
+              <img
+                src="main/request.png"
+                alt="Step 1 Icon"
+                className="w-16 h-16 mb-2"
+              />
+              <div className="text-2xl font-semibold mb-2">Fill the form</div>
+              <p className="text-gray-600">
+                Sign up as a service provider by clicking the "Become a Service Provider" button.
+              </p>
+            </div>
+
+            {/* Step 2 */}
+            <div className="flex flex-col items-center">
+              
+              <img
+                src="main/send.png"
+                alt="Step 2 Icon"
+                className="w-16 h-16 mb-2"
+              />
+              <div className="text-2xl font-semibold mb-2">Send Request</div>
+              <p className="text-gray-600">
+                Complete your profile with information about your services.
+              </p>
+            </div>
+
+            {/* Step 3 */}
+            <div className="flex flex-col items-center">
+              
+              <img
+                src="main/verified.png"
+                alt="Step 3 Icon"
+                className="w-16 h-16 mb-2"
+              />
+              <div className="text-2xl font-semibold mb-2">Get Verified</div>
+              <p className="text-gray-600">
+                Wait for approval from our team to start offering your services.
+              </p>
+            </div>
+
+            {/* Step 4 */}
+            <div className="flex flex-col items-center">
+              
+              <img
+                src="main/paid.png"
+                alt="Step 3 Icon"
+                className="w-16 h-16 mb-2"
+              />
+              <div className="text-2xl font-semibold mb-2">Earn Money</div>
+              <p className="text-gray-600">
+                Wait for approval from our team to start offering your services.
+              </p>
+            </div>
+          </div>
+      </div>
+      </div>
     </div>
   );
 };
