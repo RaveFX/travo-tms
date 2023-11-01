f32286928d26106e965124ad8c40c2d9a65667e1
Sanduni Gunawardhana <sandugunawardhana99@gmail.com>
2023-11-01 17:57:43 +0530

    profile changes

--- Client/src/pages/main/profile.jsx
+++ Client/src/pages/main/profile.jsx
@@ -165,6 +165,24 @@ function UserProfile() {
             <div className="w-full lg:w-6/12 px-4">
               <div className="relative w-full mb-3">
                 <label
+                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="DOB"
+                >
+                  Date of Birth
+                </label>
+                <input
+                  type="text"
+                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  value={userData.DOB}
+                  onChange={(event) =>
+                    setUserData({ ...userData, DOB: event.target.value })
+                  }
+                />
+              </div>
+            </div>
+            <div className="w-full lg:w-6/12 px-4">
+              <div className="relative w-full mb-3">
+                <label
                   className="block uppercase text-blueGray-600 text-xs font-bold mt-4 mb-2"
                   htmlFor="gender"
                 >
